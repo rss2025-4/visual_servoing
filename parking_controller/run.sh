c5d32de6fd7d879fd3653df849054e96690d60b3
Xinyang Chen <chenxinyang99@gmail.com>
2025-03-15 03:27:54 +0000

    tests

--- parking_controller/run.sh
+++ parking_controller/run.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
+SCRIPT_DIR=$(dirname "${BASH_SOURCE[0]}")
 
-eval "$(cd "$SCRIPT_DIR" && direnv export bash)"
+eval "$(direnv export bash)"
 
 exec python $SCRIPT_DIR/run.py
